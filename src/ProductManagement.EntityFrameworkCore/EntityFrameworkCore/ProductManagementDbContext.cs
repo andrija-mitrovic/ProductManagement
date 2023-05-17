29513adc28bf94d98a0769b11dbb0236178196a5
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2023-05-17 12:33:43 +0200

    Configure ProductManagementDbContext

--- src/ProductManagement.EntityFrameworkCore/EntityFrameworkCore/ProductManagementDbContext.cs
+++ src/ProductManagement.EntityFrameworkCore/EntityFrameworkCore/ProductManagementDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Categories;
+using ProductManagement.Products;
 using Volo.Abp.AuditLogging.EntityFrameworkCore;
 using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
 using Volo.Abp.Data;
@@ -24,7 +26,8 @@ public class ProductManagementDbContext :
     ITenantManagementDbContext
 {
     /* Add DbSet properties for your Aggregate Roots / Entities here. */
-
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
     #region Entities from the modules
 
     /* Notice: We only implemented IIdentityDbContext and ITenantManagementDbContext
@@ -73,6 +76,7 @@ public class ProductManagementDbContext :
         builder.ConfigureOpenIddict();
         builder.ConfigureFeatureManagement();
         builder.ConfigureTenantManagement();
+        builder.ConfigureProductManagement();
 
         /* Configure your own tables/entities inside here */
 
@@ -81,6 +85,28 @@ public class ProductManagementDbContext :
         //    b.ToTable(ProductManagementConsts.DbTablePrefix + "YourEntities", ProductManagementConsts.DbSchema);
         //    b.ConfigureByConvention(); //auto configure for the base class props
         //    //...
+        //});
+        //builder.Entity<Category>(b =>
+        //{
+        //    b.ToTable("Categories");
+        //    b.Property(x => x.Name)
+        //        .HasMaxLength(CategoryConsts.MaxNameLength)
+        //        .IsRequired();
+        //    b.HasIndex(x => x.Name);
+        //});
+
+        //builder.Entity<Product>(b =>
+        //{
+        //    b.ToTable("Products");
+        //    b.Property(x => x.Name)
+        //        .HasMaxLength(ProductConsts.MaxNameLength)
+        //        .IsRequired();
+        //    b.HasOne(x => x.Category)
+        //        .WithMany()
+        //        .HasForeignKey(x => x.CategoryId)
+        //        .OnDelete(DeleteBehavior.Restrict)
+        //        .IsRequired();
+        //    b.HasIndex(x => x.Name).IsUnique();
         //});
     }
 }
---
+++ src/ProductManagement.EntityFrameworkCore/EntityFrameworkCore/ProductManagementDbContextModelCreatingExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagement.Categories;
+using ProductManagement.Products;
+using Volo.Abp;
+
+namespace ProductManagement.EntityFrameworkCore
+{
+    public static class ProductManagementDbContextModelCreatingExtensions
+    {
+        public static void ConfigureProductManagement(this ModelBuilder builder)
+        {
+            Check.NotNull(builder, nameof(builder));
+
+            ////Configure your own tables/entities 
+            //builder.Entity<Book>(b =>
+            //{
+            //    b.ToTable(BookStoreConsts.DbTablePrefix + "Books", BookStoreConsts.DbSchema);
+            //    b.ConfigureByConvention(); // auto configure for the base class props
+            //    b.Property(x => x.Name).IsRequired().HasMaxLength(128);
+            //});
+
+            ConfigureCategoriesTable(builder);
+            ConfigureProductsTable(builder);
+        }
+
+        private static void ConfigureCategoriesTable(ModelBuilder builder)
+        {
+            builder.Entity<Category>(b =>
+            {
+                b.ToTable("Categories");
+                b.Property(x => x.Name)
+                    .HasMaxLength(CategoryConsts.MaxNameLength)
+                    .IsRequired();
+                b.HasIndex(x => x.Name);
+            });
+        }
+
+        private static void ConfigureProductsTable(ModelBuilder builder)
+        {
+            builder.Entity<Product>(b =>
+            {
+                b.ToTable("Products");
+                b.Property(x => x.Name)
+                    .HasMaxLength(ProductConsts.MaxNameLength)
+                    .IsRequired();
+                b.HasOne(x => x.Category)
+                    .WithMany()
+                    .HasForeignKey(x => x.CategoryId)
+                    .OnDelete(DeleteBehavior.Restrict)
+                    .IsRequired();
+                b.HasIndex(x => x.Name).IsUnique();
+            });
+        }
+    }
+}
